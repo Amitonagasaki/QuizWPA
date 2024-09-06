2afda4928c173b2950a51b39e1d036f52f8272ee
Amit Prasad <57029788+Amitonagasaki@users.noreply.github.com>
2024-09-07 03:04:41 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,14 +6,7 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAVOpKDN7TI3ObFQiapYrVVpLd_9vMAmlA",
-  authDomain: "react-quiz-32a90.firebaseapp.com",
-  projectId: "react-quiz-32a90",
-  storageBucket: "react-quiz-32a90.appspot.com",
-  messagingSenderId: "636448735297",
-  appId: "1:636448735297:web:7ec2fa58a446bc3d48c6c0",
-};
+
 // const firebaseConfig = {
 //   apiKey: import.meta.env.VITE_apiKey,
 //   authDomain: import.meta.env.VITE_authDomain,
